46bb438349344f17e4fccf2a2466a01b12182397
Fabian Loaeza <floaeza@bbinco.com>
2022-02-21 16:37:50 -0600

    DASHBOARD: Funcion para reiniciar dispositivo terminada

---
+++ public/python/LogReadDevice.py
@@ -0,0 +1,27 @@
+from telnetlib import DO
+import paramiko
+import xtelnet
+import requests
+import json
+#from datetime import datetime
+import socket
+import base64
+import sys
+
+t=xtelnet.session()
+
+parametro = ' '.join(sys.argv[1:])
+parametro = parametro.strip()
+
+parametro = parametro.split(',')
+
+if parametro[1] == 'AMINO':
+    try:
+        ip = parametro[0]
+        t.connect(ip, username='root', password='root2root', p=23, timeout=8)
+        output= t.execute('logread')
+        t.close()
+        print(output)
+    except:
+        print('DISPOSITIVO NO ALCANZADO')
+
