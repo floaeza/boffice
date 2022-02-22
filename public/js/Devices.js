c81c7341bf9cb6eba66e7613cf115455e2c39156
Fabian Loaeza <floaeza@bbinco.com>
2022-02-22 10:41:07 -0700

    TERMINAL SIMULAD: Diseño terminado y funcionalidad empezada

--- public/js/Devices.js
+++ public/js/Devices.js
@@ -1,20 +1,24 @@
 // @ts-nocheck
 
 $(document).ready(function(){
+    modalHandler();
     $('body').on('click', 'a', function(){
     //   alert($(this).attr('name'))
-        rebootDevice($(this).attr('name'));
+        if ($(this).attr('id') == 'reboot_Device') {
+            rebootDevice($(this).attr('name'));
+        }else if ($(this).attr('id') == 'logread_Device') {
+            modalHandler(true);
+            logreadDevice($(this).attr('name'));
+        }
     });
 });
-
 
 function rebootDevice(mac){
     $.ajaxSetup({
         headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
-        });
-
+    });
     $.ajax({
         type: 'GET',
         url: '/BBINCO/Device/Reboot/',
@@ -25,8 +29,82 @@ function rebootDevice(mac){
             console.log(response);
     },
     error: function(XMLHttpRequest, textStatus, errorThrown) { 
-        alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+        console.log("Status: " + textStatus); 
+        console.log("Error: " + errorThrown); 
     }  
     });
 
+}
+
+function logreadDevice(mac) {
+    var PROGAMS = {
+        help: function(...a) {
+          this.printa({
+            "help": ["get commands list"],
+            "clear": ["clear terminal"]
+          });
+        },
+        clear: function(...a) {
+          this.clear_terminal();
+        },
+        logread:function(...a) {
+            let response = getLog(mac, 'logread');
+            this.printa(response);
+          },
+      };
+    $("#terminal" ).setAsTerminal("#terminal","[root","AMINET","]","#", PROGAMS);
+}
+let modal = document.getElementById("modal");
+function modalHandler(val) {
+    if (val) {
+        fadeIn(modal);
+    } else {
+        fadeOut(modal);
+    }
+}
+function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+        if ((el.style.opacity -= 0.1) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+}
+function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "flex";
+    (function fade() {
+        let val = parseFloat(el.style.opacity);
+        if (!((val += 0.2) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+}
+function getLog(mac, command){
+    let log = ''
+    $.ajaxSetup({
+        headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+    });
+    $.ajax({
+        type: 'GET',
+        url: '/BBINCO/Device/LogRead/',
+        data: {
+            mac : mac,
+            command : command
+        },
+        success: function (response) {
+            console.log(response);
+            log = JSON.stringify(response);
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) { 
+        console.log("Status: " + textStatus); 
+        console.log("Error: " + errorThrown); 
+    }  
+    });
+    return log;
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<script src="{{asset('js/terminal_programs.js')}}"></script>
+<script src="{{asset('js/terminal.js')}}"></script>
+{{-- <link href="{{ asset('css/styles.css') }}" rel="stylesheet"> --}}
 <!-- component -->
 <div class="w-2/3 mx-auto">
     <div class="my-6 bg-white rounded shadow-md">
@@ -32,5 +35,24 @@
       {{ $devices->links() }}
     </div>
   </div>
+  <!-- component -->
+<!-- Code block starts -->
+<dh-component>
+  <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ --->
+  <div class="absolute top-0 bottom-0 left-0 right-0 z-10 py-12 transition duration-150 ease-in-out bg-gray-700" id="modal">
+      <div role="alert" class="container w-11/12 max-w-lg mx-auto md:w-2/3">
+          <div class="relative px-5 py-8 bg-white border border-gray-400 rounded shadow-md md:px-10">
+            <div id="terminal">
+              <header>>>>Welcome in terminal, type "help" to get commands</header>
+              <div id="terminal_lines">
+              </div>
+            </div>
+      </div>
+  </div>
+  {{-- <div class="flex justify-center w-full py-12" id="button">
+      <button class="px-4 py-2 mx-auto text-xs text-white transition duration-150 ease-in-out bg-indigo-700 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 hover:bg-indigo-600 sm:px-8 sm:text-sm" onclick="modalHandler(true)">Open Modal</button>
+  </div> --}}
+</dh-component>
+<!-- Code block ends -->
 <script src="{{asset('js/Devices.js')}}"></script>
 @endsection
