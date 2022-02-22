b644e9e93569ef965d80fa1187894fc66aa71402
Fabian Loaeza <floaeza@bbinco.com>
2022-02-22 11:46:50 -0600

    package

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -23,20 +23,6 @@ class DeviceController extends Controller
         $output_data = $process->getOutput();
         return response()->json([$output_data], 200);
     }
-    public function logRead(Request $request){
-        $path = public_path();
-        $path = $path.'/python/LogReadDevice.py';
-        $device = Device::where('mac_address', $request->mac)->first();
-        $arg = $device->ip_direction.','.$device->make;
-        $process = new Process(['python', $path, $arg]);
-        $process->run();
-        // error handling
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-        $output_data = $process->getOutput();
-        return response()->json([$output_data], 200);
-    }
     public function terminalDevice(Request $request){
         $path = public_path();
         $path = $path.'/python/TerminalDevice.py';
