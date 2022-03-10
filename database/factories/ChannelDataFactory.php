2b79d09c5448fbbb18af6ffa40339418bc51193c
Fabian Loaeza <floaeza@bbinco.com>
2022-03-09 18:02:00 -0700

    DB: Correccion en ChannelDataFactory

--- database/factories/ChannelDataFactory.php
+++ database/factories/ChannelDataFactory.php
@@ -24,7 +24,7 @@ class ChanneldataFactory extends Factory
             'multicast' =>$this->faker->unique()->numerify('igmp:// ###.##.#.#'), 
             'number' =>$this->faker->numberBetween(1,100),
             'port' =>$this->faker->numberBetween(1000,3000),
-            'link' =>$this->faker->randomElement(['https://www.tdtchannels.com/lists/tv.json', 'https://www.tdtchannels.com/lists/tv.m3u8']),
+            'link' =>$this->faker->url(),
             'program' =>$this->faker->numberBetween(1,200),
             'position' =>$this->faker->numberBetween(0,20),
             'audio' =>$this->faker->numberBetween(1,1000),
